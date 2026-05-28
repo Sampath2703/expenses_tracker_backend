2ab6cb3a0168f1fcd90e2f25028a43bd3d724094
Sampath2703 <naniganji2703@gmail.com>
2026-05-28 12:18:05 +0530

    updated code

--- main.py
+++ main.py
@@ -143,7 +143,7 @@ def update_expenses(expenses_id: int, updated_expenses_data: dict):
             "error": str(e)
         }
 
-@app.delete("/delete_expenses/{expense_id}")
+@app.delete("/delete_expense/{expense_id}")
 def delete_expense(expense_id: int):
 
     query = "DELETE FROM expenses1 WHERE expense_id=%s"
